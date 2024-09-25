9d7494f7b239ed3a0bd9fb38c0f2c193cc1c7142
Davi-bybo <davigomes17122005@gmail.com>
2024-09-25 09:58:05 -0300

    Add files via upload

---
+++ javascript/comparacao/js/script.js
@@ -0,0 +1,40 @@
+//comparação
+const numeroA = 2;
+const numeroB = 8;
+console.log(numeroA < numeroB);
+console.log(numeroA > numeroB);
+
+
+const numeroC = 20;
+const numeroD = 20;
+console.log(numeroC >= numeroD);
+
+//comparação com igualdade
+const a = 2;
+const b = 2;
+console.log(a === b); //valor e tipo
+
+//comparação commigualdade ampla
+const c = 10;
+const d = "10";
+console.log(c == d); //valor e converter o tipo
+
+//diferenciação estrita
+const e = 3;
+const f = 4;
+console.log(e !== f);
+
+//direfenciação ampla
+const g = 1;
+const h = "1"; //converte isso para inteiro = 1
+console.log(g != h); // 1 é difernte de 1
+
+//empty values
+let x; 
+let y = null;
+let z = "";
+console.log(x);
+console.log(y);
+console.log("");
+
+//ob
---
+++ javascript/objetodoconsole/js/scritp.js
@@ -0,0 +1,18 @@
+//Exibindo uma mensagem de aviso na console
+console.warn("Atençao; Aviso");
+
+//Exibindo uma mensagemd de erro do console
+console.error("Ocorreu erro");
+
+//Exibindo uma mensagem informativa no console
+console.info("informativa");
+
+//Criando um grupo de mensagem no consele
+console.group("Mensagem de teste");
+console.log("Atente-se ao passo 1");
+console.log("Execute o passo 2");
+console.log("Execute o passo final");
+console.groupEnd();
+
+//Limpar o console
+//console.clear();
